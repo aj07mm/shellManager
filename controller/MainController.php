e69bbe59abff8ec0eaa37dd85286562513bb4425
Diego Ramos <rdiego26@gmail.com>
2014-06-24 23:59:58 -0300

    CLOSE #2  utilizando fread ao invés de readFile

--- controller/MainController.php
+++ controller/MainController.php
@@ -8,16 +8,19 @@ class MainController{
 	}
 
 	public function actionIndex(){
-
 		//$this->render('index.php');
 	}
 
-	public function getScript($filename){
+	public function getScript($filename) {
 
 		$filepath = self::SCRIPTS_PATH.'/'.$filename;
-		if(file_exists($filepath)){
-			echo readfile($filepath);
-		}else
+
+		if(file_exists($filepath)) {
+            $fh = fopen($filepath, "r");
+            $data = fread($fh, filesize($filepath));
+            fclose($fh);
+            echo $data;
+		} else
 			echo false;
 	}
 
